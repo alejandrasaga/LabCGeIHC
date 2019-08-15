a61f413a7268df5e76e5e979bdd477b627b23ada
alejandrasaga <salasgalejandra@gmail.com>
2019-08-15 16:56:54 -0500

    Practica 01

--- 01-ManejoVentanasGLFW/src/main.cpp
+++ 01-ManejoVentanasGLFW/src/main.cpp
@@ -20,6 +20,8 @@ bool exitApp = false;
 int lastMousePosX;
 int lastMousePosY;
 
+int colorStatus = 0;
+
 double deltaTime;
 
 // Se definen todos las funciones.
@@ -79,7 +81,7 @@ void init(int width, int height, std::string strTitle, bool bFullScreen) {
 	}
 
 	glViewport(0, 0, screenWidth, screenHeight);
-	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+	glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
 }
 
 void destroy() {
@@ -99,6 +101,15 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode
 		case GLFW_KEY_ESCAPE:
 			exitApp = true;
 			break;
+		case GLFW_KEY_R:
+			colorStatus = 0;
+			break;
+		case GLFW_KEY_G:
+			colorStatus = 1;
+			break;
+		case GLFW_KEY_B:
+			colorStatus = 2;
+			break;
 		}
 	}
 }
@@ -113,9 +124,12 @@ void mouseButtonCallback(GLFWwindow* window, int button, int state, int mod) {
 		switch (button) {
 		case GLFW_MOUSE_BUTTON_RIGHT:
 			std::cout << "lastMousePos.y:" << lastMousePosY << std::endl;
+			glClearColor(0.3, 0.0, 0.4, 0.0);
 			break;
 		case GLFW_MOUSE_BUTTON_LEFT:
 			std::cout << "lastMousePos.x:" << lastMousePosX << std::endl;
+			//glClearColor(0.1, 0.0, 0.3, 0.0);
+			colorStatus = 20;
 			break;
 		case GLFW_MOUSE_BUTTON_MIDDLE:
 			std::cout << "lastMousePos.x:" << lastMousePosX << std::endl;
@@ -139,12 +153,23 @@ void applicationLoop() {
 	while (psi) {
 		psi = processInput(true);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		if (colorStatus == 0)
+			glClearColor(1.0, 0.0f, 1.0, 0.0);
+		else if (colorStatus == 1)
+			glClearColor(0.0, 1.0, 0.0, 0.0);
+		else if (colorStatus == 2)
+			glClearColor(0.0, 0.0, 1.0, 0.0);
+
+		else if (colorStatus == 20)
+			glClearColor(0.3, 0.0, 0.2, 0.0);
+
 		glfwSwapBuffers(window);
 	}
 }
 
 int main(int argc, char ** argv) {
-	init(800, 700, "Window GLFW", false);
+	init(1600, 900, "Mi primer ventana", true);
 	applicationLoop();
 	destroy();
 	return 1;
